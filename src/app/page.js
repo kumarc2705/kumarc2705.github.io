b7f1471f91c4dcdfe9082c40f00d60418c7c83b1
Kumar Chandrasekhar <kumarc2705@gmail.com>
2023-11-27 20:56:24 -0600

    node-fetch

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@ import Image from 'next/image'
 import HeroSection from "./components/HeroSection";
 import Navbar from './components/Navbar';
 const minimatch = require('minimatch');
+import fetch, {Headers} from 'node-fetch'
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch
+  globalThis.Headers = Headers
+}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
